d199d7cdabb386e55c827d634a5e5a27705cc2d7
alexb105 <alexanderbonnici214@gmail.com>
2025-12-06 22:10:42 +0000

    Add missing use-dnd-sensors hook

---
+++ hooks/use-dnd-sensors.ts
@@ -0,0 +1,28 @@
+import {
+  KeyboardSensor,
+  PointerSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core"
+import { sortableKeyboardCoordinates } from "@dnd-kit/sortable"
+
+export function useDndSensors() {
+  return useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 8,
+      },
+    }),
+    useSensor(TouchSensor, {
+      activationConstraint: {
+        delay: 200,
+        tolerance: 5,
+      },
+    }),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  )
+}
+
